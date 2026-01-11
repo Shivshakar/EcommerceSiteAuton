6ea6c9f18651e0c78f9c0cc4558ad30fab6484a6
Shivshakar <shivshankarpatil1996@gmail.com>
2026-01-11 23:37:27 +0530

    chore: save workspace changes

--- src/test/java/com/automation/tests/SignupLoginUserTest.java
+++ src/test/java/com/automation/tests/SignupLoginUserTest.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegisterUserTest extends TestBase {
+public class SignupLoginUserTest extends TestBase {
 
     // store credentials so dependent test can use them
     private static String registeredEmail;
